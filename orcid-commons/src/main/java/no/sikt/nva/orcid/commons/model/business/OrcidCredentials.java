28288f7bb007906042ce06be5a037684a56e2e69
anetteOlli <anette.olli.siiri@sikt.no>
2023-04-27 11:26:27 +0200

    NP-44636-orcid-locale-test-utils, constants

--- orcid-commons/src/main/java/no/sikt/nva/orcid/commons/model/business/OrcidCredentials.java
+++ orcid-commons/src/main/java/no/sikt/nva/orcid/commons/model/business/OrcidCredentials.java
@@ -18,26 +18,35 @@ import lombok.Data;
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 public class OrcidCredentials {
 
-    @JsonProperty("orcid")
+    private static final String ORCID = "orcid";
+    private static final String ACCESS_TOKEN = "access_token";
+    private static final String TOKEN_TYPE = "token_type";
+    private static final String EXPIRES_IN = "expires_in";
+    private static final String TOKEN_VERSION = "tokenVersion";
+    private static final String PERSISTENT = "persistent";
+    private static final String ID_TOKEN = "id_token";
+    private static final String TOKEN_ID = "tokenId";
+
+    @JsonProperty(ORCID)
     private final URI orcid;
-    @JsonProperty("access_token")
+    @JsonProperty(ACCESS_TOKEN)
     private final String accessToken;
 
-    @JsonProperty("token_type")
+    @JsonProperty(TOKEN_TYPE)
     private final String tokenType;
 
-    @JsonProperty("expires_in")
+    @JsonProperty(EXPIRES_IN)
     private final int expiresIn;
 
-    @JsonProperty("tokenVersion")
+    @JsonProperty(TOKEN_VERSION)
     private final String tokenVersion;
 
-    @JsonProperty("persistent")
+    @JsonProperty(PERSISTENT)
     private final boolean persistent;
 
-    @JsonProperty("id_token")
+    @JsonProperty(ID_TOKEN)
     private final String idToken;
 
-    @JsonProperty("tokenId")
+    @JsonProperty(TOKEN_ID)
     private final int tokenId;
 }
