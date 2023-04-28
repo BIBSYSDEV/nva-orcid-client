93c10f4b946deea34787a0b752d2d70c3cc27610
anetteOlli <anette.olli.siiri@sikt.no>
2023-04-28 14:18:34 +0200

    NP-44638-handler-for-storing-credentials, added default constructor

--- orcid-rest/src/main/java/no/sikt/nva/orcid/handlers/StoreOrcidCredentialsFunction.java
+++ orcid-rest/src/main/java/no/sikt/nva/orcid/handlers/StoreOrcidCredentialsFunction.java
@@ -1,16 +1,21 @@
 package no.sikt.nva.orcid.handlers;
 
 import static nva.commons.core.attempt.Try.attempt;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import java.net.HttpURLConnection;
+import java.time.Clock;
 import no.sikt.nva.orcid.commons.model.business.OrcidCredentials;
 import no.sikt.nva.orcid.commons.model.exceptions.TransactionFailedException;
 import no.sikt.nva.orcid.commons.service.OrcidService;
+import no.sikt.nva.orcid.commons.service.OrcidServiceImpl;
 import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 import nva.commons.apigateway.exceptions.BadGatewayException;
 import nva.commons.apigateway.exceptions.ConflictException;
+import nva.commons.core.Environment;
+import nva.commons.core.JacocoGenerated;
 import nva.commons.core.attempt.Failure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,7 +25,16 @@ public class StoreOrcidCredentialsFunction extends ApiGatewayHandler<OrcidCreden
     private static final String COULD_NOT_STORE_CREDENTIALS = "Could not store credentials";
     private static final Logger logger = LoggerFactory.getLogger(StoreOrcidCredentialsFunction.class);
     private static final String ORCID_CREDENTIALS_ALREADY_EXISTS = "Orcid credentials already exists";
+    private static final String TABLE_NAME = "TABLE_NAME";
     private final OrcidService orcidService;
+
+    @JacocoGenerated
+    public StoreOrcidCredentialsFunction() {
+        this(new OrcidServiceImpl(
+            new Environment().readEnv(TABLE_NAME),
+            AmazonDynamoDBClientBuilder.defaultClient(),
+            Clock.systemDefaultZone()));
+    }
 
     public StoreOrcidCredentialsFunction(OrcidService orcidService) {
         super(OrcidCredentials.class);
