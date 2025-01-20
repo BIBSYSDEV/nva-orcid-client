b657377391ec006e0a2202c2255d6445de78f4e1
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-01-20 15:14:52 +0100

    build: Upgrade workflow and java (#50)
    Changes:
    
        Upgrades to Java 21 and Gradle 8.12
        Upgrades PMD and fixes invalid rules
        Replaces GitHub workflows with reference to shared workflow
        Removes lombok from the project

--- orcid-commons/src/main/java/no/sikt/nva/orcid/commons/model/business/OrcidCredentials.java
+++ orcid-commons/src/main/java/no/sikt/nva/orcid/commons/model/business/OrcidCredentials.java
@@ -2,42 +2,109 @@ package no.sikt.nva.orcid.commons.model.business;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import lombok.Builder;
-import no.unit.nva.commons.json.JsonSerializable;
-
 import java.net.URI;
 import java.time.Instant;
 import java.util.Objects;
+import no.unit.nva.commons.json.JsonSerializable;
 
-
-@Builder(
-        builderClassName = "OrcidCredentialsBuilder",
-        toBuilder = true,
-        builderMethodName = "builder",
-        buildMethodName = "build",
-        setterPrefix = "with"
-)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @JsonSerialize
-public record OrcidCredentials(URI orcid,
-                               String accessToken,
-                               String tokenType,
-                               int expiresIn,
-                               String tokenVersion,
-                               boolean persistent,
-                               String idToken,
-                               int tokenId,
-                               Instant modified,
-                               Instant created) implements JsonSerializable {
+public record OrcidCredentials(URI orcid, String accessToken, String tokenType, int expiresIn, String tokenVersion,
+                               boolean persistent, String idToken, int tokenId, Instant modified, Instant created)
+    implements JsonSerializable {
+
+    public static Builder builder() {
+        return new Builder();
+    }
 
     public boolean hasSameCredentials(OrcidCredentials credentials) {
         return expiresIn == credentials.expiresIn
-                && persistent == credentials.persistent
-                && tokenId == credentials.tokenId
-                && Objects.equals(orcid, credentials.orcid)
-                && Objects.equals(accessToken, credentials.accessToken)
-                && Objects.equals(tokenType, credentials.tokenType)
-                && Objects.equals(tokenVersion, credentials.tokenVersion)
-                && Objects.equals(idToken, credentials.idToken);
+               && persistent == credentials.persistent
+               && tokenId == credentials.tokenId
+               && Objects.equals(orcid, credentials.orcid)
+               && Objects.equals(accessToken, credentials.accessToken)
+               && Objects.equals(tokenType, credentials.tokenType)
+               && Objects.equals(tokenVersion, credentials.tokenVersion)
+               && Objects.equals(idToken, credentials.idToken);
+    }
+
+    public static final class Builder {
+
+        private URI orcid;
+        private String accessToken;
+        private String tokenType;
+        private int expiresIn;
+        private String tokenVersion;
+        private boolean persistent;
+        private String idToken;
+        private int tokenId;
+        private Instant modified;
+        private Instant created;
+
+        private Builder() {
+        }
+
+        public Builder withOrcid(URI orcid) {
+            this.orcid = orcid;
+            return this;
+        }
+
+        public Builder withAccessToken(String accessToken) {
+            this.accessToken = accessToken;
+            return this;
+        }
+
+        public Builder withTokenType(String tokenType) {
+            this.tokenType = tokenType;
+            return this;
+        }
+
+        public Builder withExpiresIn(int expiresIn) {
+            this.expiresIn = expiresIn;
+            return this;
+        }
+
+        public Builder withTokenVersion(String tokenVersion) {
+            this.tokenVersion = tokenVersion;
+            return this;
+        }
+
+        public Builder withPersistent(boolean persistent) {
+            this.persistent = persistent;
+            return this;
+        }
+
+        public Builder withIdToken(String idToken) {
+            this.idToken = idToken;
+            return this;
+        }
+
+        public Builder withTokenId(int tokenId) {
+            this.tokenId = tokenId;
+            return this;
+        }
+
+        public Builder withModified(Instant modified) {
+            this.modified = modified;
+            return this;
+        }
+
+        public Builder withCreated(Instant created) {
+            this.created = created;
+            return this;
+        }
+
+        public OrcidCredentials build() {
+            return new OrcidCredentials(orcid,
+                                        accessToken,
+                                        tokenType,
+                                        expiresIn,
+                                        tokenVersion,
+                                        persistent,
+                                        idToken,
+                                        tokenId,
+                                        modified,
+                                        created);
+        }
     }
 }
--- orcid-rest/src/main/java/no/sikt/nva/orcid/model/CristinPersonResponse.java
+++ orcid-rest/src/main/java/no/sikt/nva/orcid/model/CristinPersonResponse.java
@@ -1,38 +1,17 @@
 package no.sikt.nva.orcid.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.List;
+import java.util.Collection;
 import java.util.Optional;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import nva.commons.core.JacocoGenerated;
 
-@Data
-@Builder(
-    builderClassName = "CristinPersonResponseBuilder",
-    toBuilder = true,
-    builderMethodName = "builder",
-    buildMethodName = "build",
-    setterPrefix = "with"
-)
-@AllArgsConstructor(access = AccessLevel.PACKAGE)
-public class CristinPersonResponse {
-
-    @JsonProperty("identifiers")
-    private List<CristinPersonIdentifier> identifiers;
-
-    @JacocoGenerated
-    public CristinPersonResponse() {
-    }
+public record CristinPersonResponse(Collection<CristinPersonIdentifier> identifiers) {
 
     @JsonIgnore
     public Optional<String> getOrcid() {
-        return identifiers.stream()
+        return identifiers
+                   .stream()
                    .filter(CristinPersonIdentifier::isOrcid)
-                   .map(CristinPersonIdentifier::getValue)
+                   .map(CristinPersonIdentifier::value)
                    .findFirst();
     }
 }
