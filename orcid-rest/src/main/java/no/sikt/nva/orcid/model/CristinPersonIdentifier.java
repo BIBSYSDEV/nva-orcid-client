d0aa27323338894fe9010cbb51569ec84a185a60
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-01-20 13:25:46 +0100

    build: Upgrade to Java 21
    Also removes lombok and bumps PMD

--- orcid-rest/src/main/java/no/sikt/nva/orcid/model/CristinPersonIdentifier.java
+++ orcid-rest/src/main/java/no/sikt/nva/orcid/model/CristinPersonIdentifier.java
@@ -1,32 +1,8 @@
 package no.sikt.nva.orcid.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import nva.commons.core.JacocoGenerated;
 
-@Data
-@Builder(
-    builderClassName = "CristinPersonIdentifierBuilder",
-    toBuilder = true,
-    builderMethodName = "builder",
-    buildMethodName = "build",
-    setterPrefix = "with"
-)
-@AllArgsConstructor(access = AccessLevel.PACKAGE)
-public class CristinPersonIdentifier {
-
-    @JsonProperty("type")
-    private String type;
-    @JsonProperty("value")
-    private String value;
-
-    @JacocoGenerated
-    public CristinPersonIdentifier() {
-    }
+public record CristinPersonIdentifier(String type, String value) {
 
     @JsonIgnore
     public boolean isOrcid() {
